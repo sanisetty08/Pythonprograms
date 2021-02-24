ed0dfcc2bc83308dab9285f4850135ade5f07441
Srinivas Anisetty <srinivasanisetty08@gmail.com>
2021-02-23 20:14:20 -0500

    adding functions user_input
    Signed-off-by: Srinivas Anisetty <srinivasanisetty08@gmail.com>

--- udemy/function_2.py
+++ udemy/function_2.py
@@ -23,3 +23,47 @@ elif (meaner(list_3) > 15):
     print(list_3)
 else:
     print("none")
+
+
+#######################################################################################################
+def password_controller(E):
+    if len(E) < 8:
+        return False
+    else:
+        return True
+
+
+print(password_controller("mypass"))
+print(password_controller("mylingpassword"))
+# method2
+password_controller_var = password_controller("mypass")
+print(password_controller_var)
+
+
+###########################################################################################################
+def temperature(E):
+    if (E > 7):
+        return "Warm"
+    else:
+        return "Cold"
+
+
+print(temperature(10))
+print(temperature(7))
+
+
+###########################################################################################################
+def temperature(E):
+    if (E > 25):
+        return "Hot"
+    elif (E >= 15) and (E <= 25):
+        return "Warm"
+    else:
+        return "Cold"
+
+
+# print(temperature(10))
+# print(temperature(70))
+# user_input = int(input("Enter some number:"))
+user_input = float(input("please enter some temperature number:"))
+print(temperature(user_input))
