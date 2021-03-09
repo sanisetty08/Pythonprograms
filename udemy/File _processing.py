81f7e17439f4eb5b96c3dd1927753d7f3e1dba49
Srinivas Anisetty <srinivasanisetty08@gmail.com>
2021-03-08 20:25:54 -0500

    file processing write_read
    Signed-off-by: Srinivas Anisetty <srinivasanisetty08@gmail.com>

---
+++ udemy/File _processing.py
@@ -0,0 +1,28 @@
+myfile = open("D:\Pysql\Pythonprograms\Fruits.txt")
+print(myfile.read())
+
+
+####################################################################################################
+def foo(character, filepath):
+    file = open(filepath)
+    content = file.read()
+    return content.count(character)
+
+
+print(foo("a", "D:\Pysql\Pythonprograms\Fruits.txt"))
+
+#####################################################################################################
+file = open("D:\Pysql\Pythonprograms\Fruits.txt")
+content = file.read()
+
+with open("D:\Pysql\Pythonprograms\snail.txt", "w") as file:
+    file.write("snail\n")
+    file.write(content[:3])
+
+####################################################################################################
+with open("D:\Pysql\Pythonprograms\Fruits.txt", "a+") as file:
+    file.seek(0)
+    content = file.read()
+    file.seek(0)
+    file.write("\ncontent")
+    file.write("\ncontent")
