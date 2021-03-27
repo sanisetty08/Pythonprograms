ee2927f733647d1f9397f71beefe922d725a7220
sanisetty08 <srinivasanisetty08@gmail.com>
2021-03-28 02:25:14 +0530

    stringformating3_28
    Signed-off-by: sanisetty08 <srinivasanisetty08@gmail.com>

---
+++ udemy/stringformating.py
@@ -0,0 +1,8 @@
+phone_numbers = ['37682929928', '423998200919']
+flat_number = ['401', '404']
+for phone_numbers, flat_number in zip(phone_numbers, flat_number):
+    print("phone number=" + str(phone_numbers) + " and flat number=" + str(flat_number))
+
+# phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+# for key, value in phone_numbers.items():
+# print("{}: {}" .format(key,value))
