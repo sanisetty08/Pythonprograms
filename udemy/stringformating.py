1aa9aad34edcd599b8c3750879f5c3c4cf89dec8
sanisetty08 <srinivasanisetty08@gmail.com>
2021-03-28 02:42:57 +0530

    stringformating_and_loop_3_28
    Signed-off-by: sanisetty08 <srinivasanisetty08@gmail.com>

--- udemy/stringformating.py
+++ udemy/stringformating.py
@@ -1,8 +1,13 @@
+# loop with two variables with string formating
 phone_numbers = ['37682929928', '423998200919']
 flat_number = ['401', '404']
-for phone_numbers, flat_number in zip(phone_numbers, flat_number):
-    print("phone number=" + str(phone_numbers) + " and flat number=" + str(flat_number))
+# for phone_numbers, flat_number in zip(phone_numbers, flat_number):
+# print("phone number=" + str(phone_numbers) + " and flat number=" + str(flat_number))
 
+for phone_numbers, flat_number in zip(phone_numbers, flat_number):
+    print("phone number= %s and flat number= %s" % (phone_numbers, flat_number))
+
+# loop with dictionaries
 # phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
 # for key, value in phone_numbers.items():
 # print("{}: {}" .format(key,value))
